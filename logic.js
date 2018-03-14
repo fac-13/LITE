75a7a05dc70356d54635687b5c0210600d512356
isnotafunction <ivana.cosgrove@gmail.com>
2018-03-14 16:03:37 +0000

    adds test for extractKeywords checking for 0 true values and passes

--- logic.js
+++ logic.js
@@ -1,17 +1,28 @@
 
 var logic = {
- 
 
-  extractKeywords: function(){
 
-  }
+  extractKeywords: function(obj){
+    var asteroidArray = obj.near_earth_objects["2018-01-04"];
+    var result = asteroidArray.map(function(val){
+      return val.is_potentially_hazardous_asteroid
+    })
+    var trueValues = result.filter(function(val){
+      return val===true;
+    })
+    if(trueValues.length == 0){
+      return ["happy"]
+    } else {
+      return "hi"
+    }
+  },
 
   extractData: function(){
 
-  }
+  },
 
   processDateFormat: function(){
-    
+
   }
 
 
--- test.js
+++ test.js
@@ -3,3 +3,11 @@ var logic = require("./logic");
 var responses = require("./test-double");
 var willHitObj = responses.willHit;
 var wontHitObj = responses.wontHit;
+
+//testing extractKeywords fn
+test("tests keywords are extracted based on object data", function(t){
+  var actual = logic.extractKeywords(wontHitObj);
+  var expected = ["happy"];
+  t.deepEqual(actual, expected, "0 trues - should return ['happy']")
+  t.end();
+})
