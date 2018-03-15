078d8d523a52f466d4223b220d2b84ad1e4bb668
tspeed90 <tspeed90@gmail.com>
2018-03-15 11:44:49 +0000

    added vars for user input, mid-process for fetch

--- dom.js
+++ dom.js
@@ -1,3 +1,12 @@
+var input = document.querySelector('#date');
+var submitButton = document.querySelector('#submit');
+var date = input.value;
+var nasaDate = 'start_date=' + date + '&end_date='+ date;
+//var nasaKey = '&api_key=' + config.nasaKey;
+//var nasaURL = 'https://api.nasa.gov/neo/rest/v1/feed?' + nasaDate + nasaKey;
+//var giphyKey = '&api_key=' + config.giphyKey;
+var giphyKeyword;
+//var giphyURL = 'http://api.giphy.com/v1/gifs/search?q=' + giphyKeyword + giphyKey;
 
 // fetch to be used as a general function for calling an api request...
 
@@ -5,19 +14,27 @@ function fetch(url, callback, other) {
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("load", function() {
   if (xhr.status === 200) {
-  var response = JSON.parse(xhr.responseText);
-  return callback(response);
+    var response = JSON.parse(xhr.responseText);
+    return callback(response);
   } else {
-  other();
+    other();
   }
   });
   xhr.open("GET", url);
   xhr.send();
-  }
-  
-  // function for eventlisteners
-  function addListener(selector, eventName, callback) {
-  document.querySelector(selector).addEventListener(eventName, callback);
-  }
+}
 
-  function displayData(){}
+// function for eventlisteners
+function addListener(selector, eventName, callback) {
+selector.addEventListener(eventName, callback);
+}
+
+function displayData(){
+  // one process to determine what is inside of the object passed in
+}
+
+addListener(submitButton, 'click', function(e) {
+  console.log(e);
+});
+
+  //potentially have displayData() take two arrays, one with objects and one with strings
